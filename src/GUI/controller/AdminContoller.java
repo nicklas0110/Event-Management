2c541e5f2efcf365713ef12c34258b7445c37b0d
FrederikReigstad <Frederik9876@gmail.com>
2022-03-21 21:37:41 +0100

    made id's and onAction on Importen stuff

--- src/GUI/controller/AdminContoller.java
+++ src/GUI/controller/AdminContoller.java
@@ -1,4 +1,30 @@
 package GUI.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class AdminContoller {
+    public TableView adminEventMangerTableView;
+    public TableColumn adminEventMangerTableViewName;
+    public TableColumn adminEventMangerTableViewLastname;
+    public TableColumn adminEventMangerTableViewEmail;
+    public TableColumn adminEventMangerTableViewEvents;
+    public JFXButton createEventManger;
+    public JFXButton editEventManger;
+    public JFXButton deletEventManger;
+    public JFXButton logOutAdmin;
+
+    public void createEventMangerBtn(ActionEvent event) {
+    }
+
+    public void editEventMangerBtn(ActionEvent event) {
+    }
+
+    public void deletEventMangerBtn(ActionEvent event) {
+    }
+
+    public void logOutAdminBtn(ActionEvent event) {
+    }
 }
