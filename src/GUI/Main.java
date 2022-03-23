6b81fe7626b14774ba1693f9abd110d16ce80d4d
MostafaHeidari <70130828+MostafaHeidari@users.noreply.github.com>
2022-03-23 17:24:33 +0100

    just the GUI is design now, that the event coordinator able to makes a new event and delete an event.

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/GUI/View/MainLoginView.fxml"));
-        primaryStage.setTitle("EventView");
+        primaryStage.setTitle("Log in Event Management");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
