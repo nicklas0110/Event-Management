029634b29787e27f1031b374631a734b812d1afa
FrederikReigstad <Frederik9876@gmail.com>
2022-03-24 11:24:59 +0100

    Navagatin in the admin page

--- src/GUI/controller/NewEventMangerController.java
+++ src/GUI/controller/NewEventMangerController.java
@@ -1,4 +1,11 @@
 package GUI.controller;
 
+import javafx.event.ActionEvent;
+
 public class NewEventMangerController {
+    public void createEventMangerBtn(ActionEvent actionEvent) {
+    }
+
+    public void newEventMangerCancelBtn(ActionEvent actionEvent) {
+    }
 }
