7dafcd2fd0f7d94eb2019901fa44c1d0b4de0c38
MostafaHeidari <70130828+MostafaHeidari@users.noreply.github.com>
2022-04-01 13:39:17 +0200

    get og set til Customer.java

---
+++ src/Customer.java
@@ -0,0 +1,55 @@
+public class Customer {
+    private int id;
+    private String Name;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+
+    public Customer(int id, String name, String lastName, String phoneNumber, String email) {
+        this.id = id;
+        Name = name;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
